b65615689a05eee2a3dfed2c5b1cf739ec186b27
Edgar Soares Martinez Moya <edxgar48@gmail.com>
2023-08-21 06:22:21 -0300

    anexei mais dois exercicios

---
+++ dotenv.dart
@@ -0,0 +1,11 @@
+// DotEnv é um formato de arquivo de texto muito utilizado no backend para armazenar valores por chave e valor, ex:
+// DATABASE_URL="http://DATABASE"
+// IS_ADMIN=true
+// REFRESH_TIME=123454 # second
+// Crie uma classe que leia um arquivo '.env' e disponibilize a leitura dos dados pelas chaves lembrando de prevalecer os tipos primitivos.
+
+// REGRAS:
+
+// - Chaves e valores são separados pelo sinal de igual (=).
+
+// - Arquivos DotEnv geralmente tem comentários que começam com HashTag (#). Isso deve ser ignorado.
